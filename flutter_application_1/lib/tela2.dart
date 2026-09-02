1e590fb42ced463917236fd2515ecc6c2fa5db4d
PedroGimenes <phgimenes.andrade@gmail.com>
2026-09-02 14:00:04 -0300

    aura

--- flutter_application_1/lib/tela2.dart
+++ flutter_application_1/lib/tela2.dart
@@ -3,10 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(const MaterialApp(
-    home: Tela2(),
-    debugShowCheckedModeBanner: false,
-  ));
+  runApp(const MaterialApp(home: Tela2(), debugShowCheckedModeBanner: false));
 }
 
 class Tela2 extends StatefulWidget {
@@ -17,10 +14,22 @@ class Tela2 extends StatefulWidget {
 }
 
 class _TelaPostState extends State<Tela2> {
-  // 1. Iniciam vazias
-  String titulo = '';
-  String conteudo = '';
-  bool carregando = true; // Controla se ainda está buscando na internet
+  // Variáveis para armazenar os dados do CEP
+  String cep = '';
+  String logradouro = '';
+  String complemento = '';
+  String unidade = '';
+  String bairro = '';
+  String localidade = '';
+  String uf = '';
+  String estado = '';
+  String regiao = '';
+  String ibge = '';
+  String gia = '';
+  String ddd = '';
+  String siafi = '';
+
+  bool carregando = true;
 
   @override
   void initState() {
@@ -29,17 +38,29 @@ class _TelaPostState extends State<Tela2> {
   }
 
   Future<void> buscarPost() async {
-    final url = Uri.parse('viacep.com.br/ws/01001000/json/');
+    final url = Uri.parse('https://viacep.com.br/ws/01001000/json/');
 
     final resposta = await http.get(url);
 
     if (resposta.statusCode == 200) {
       final dados = jsonDecode(resposta.body);
 
-      // 2. Atualiza os dados e avisa que terminou de carregar
+      // Atualiza os dados recebidos da API
       setState(() {
-        titulo = dados['title'];
-        conteudo = dados['body'];
+        cep = dados['cep'];
+        logradouro = dados['logradouro'];
+        complemento = dados['complemento'];
+        unidade = dados['unidade'];
+        bairro = dados['bairro'];
+        localidade = dados['localidade'];
+        uf = dados['uf'];
+        estado = dados['estado'];
+        regiao = dados['regiao'];
+        ibge = dados['ibge'];
+        gia = dados['gia'];
+        ddd = dados['ddd'];
+        siafi = dados['siafi'];
+
         carregando = false;
       });
     }
@@ -47,11 +68,10 @@ class _TelaPostState extends State<Tela2> {
 
   @override
   Widget build(BuildContext context) {
-    // 3. Enquanto estiver carregando, mostra a bolinha rodando
     if (carregando) {
       return Scaffold(
         appBar: AppBar(
-          title: const Text('Consulta JSON padrão - 3F'),
+          title: const Text('Consulta CEP - 3F'),
         ),
         body: const Center(
           child: CircularProgressIndicator(),
@@ -59,10 +79,9 @@ class _TelaPostState extends State<Tela2> {
       );
     }
 
-    // 4. Quando o carregando virar false, ele mostra a tela com os dados
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Consulta JSON padrão - 3F'),
+        title: const Text('Consulta CEP - 3F'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
@@ -70,16 +89,57 @@ class _TelaPostState extends State<Tela2> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const Text(
-              'Título:',
-              style: TextStyle(fontWeight: FontWeight.bold),
+              'CEP:',
             ),
-            Text(titulo),
-            const SizedBox(height: 10),
+            Text(cep),
             const Text(
-              'Conteúdo:',
-              style: TextStyle(fontWeight: FontWeight.bold),
+              'Logradouro:',
             ),
-            Text(conteudo),
+            Text(logradouro),
+            const Text(
+              'Complemento:',
+            ),
+            Text(complemento),
+            const Text(
+              'Unidade:',
+            ),
+            Text(unidade),
+            const Text(
+              'Bairro:',
+            ),
+            Text(bairro),
+            const Text(
+              'Localidade:',
+            ),
+            Text(localidade),
+            const Text(
+              'UF:',
+            ),
+            Text(uf),
+            const Text(
+              'Estado:',
+            ),
+            Text(estado),
+            const Text(
+              'Região:',
+            ),
+            Text(regiao),
+            const Text(
+              'IBGE:',
+            ),
+            Text(ibge),
+            const Text(
+              'GIA:',
+            ),
+            Text(gia),
+            const Text(
+              'DDD:',
+            ),
+            Text(ddd),
+            const Text(
+              'SIAFI:',
+            ),
+            Text(siafi),
           ],
         ),
       ),
